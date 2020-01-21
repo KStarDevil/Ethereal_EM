a426e5159fba612cf531d279fdf903831954daa4
Shine Wanna Aung <shinewanna27199@gmail.com>
2020-01-21 13:20:12 +0630

    sql

--- Dump20200121.sql
+++ Dump20200121.sql
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `tbl_admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tbl_admin` (
-  `admin_id` int(50) NOT NULL,
+  `admin_id` int(50) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(45) DEFAULT NULL,
   `admin_password` varchar(45) DEFAULT NULL,
   `admin_photo` text,
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `tbl_admin` (
 
 LOCK TABLES `tbl_admin` WRITE;
 /*!40000 ALTER TABLE `tbl_admin` DISABLE KEYS */;
+INSERT INTO `tbl_admin` VALUES (1,'DJ','123456','black'),(2,'Hyper','654321','gay'),(3,'Oeshi','13579','nay'),(4,'Bunny','24680','Good');
 /*!40000 ALTER TABLE `tbl_admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -168,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-21 11:01:22
+-- Dump completed on 2020-01-21 13:19:21
