4d5b26b067f90f3c3536351e0ac903936c81e234
Kaung Myat Noe <obamese.969@gmail.com>
2020-01-28 10:50:36 +0630

    Poat_Controller fixed

--- Controllers/Post_Controller.cs
+++ Controllers/Post_Controller.cs
@@ -33,15 +33,15 @@ namespace Ethereal_EM
             {
                 // dynamic dd = param;
                 // int id = dd.id;
-                dynamic mainQuery1 = _repositoryWrapper.Post_Repository.GetPost();
-                dynamic mainQuery2 = _repositoryWrapper.Post_Detail_Repository.GetPostDetail();
-                if (mainQuery1 == null && mainQuery2 == null)
+                dynamic PostData = _repositoryWrapper.Post_Repository.GetPost();
+                dynamic PostDetailData = _repositoryWrapper.Post_Detail_Repository.GetPostDetail();
+                if (PostData == null && PostDetailData == null)
                 {
-                    jsondata = new { data = new { mainQuery = "No Data" } };
+                    jsondata = new { data = new { status = 0, Message = "No Data" } };
                 }
                 else
                 {
-                    jsondata = new { data = new { mainQuery1,mainQuery2 } };
+                    jsondata = new { data = new { status = 1, Message = "Success", PostData, PostDetailData } };
                 }
             }
             catch (Exception ex)
@@ -54,39 +54,40 @@ namespace Ethereal_EM
         [HttpPost("SavePost", Name = "SavePost")]
         public dynamic SavePost([FromBody] Newtonsoft.Json.Linq.JObject param)
         {
-            string save ="";
+            string save = "";
             try
             {
                 dynamic dd = param;
                 int id = dd.post_id;
 
                 string user_photo = dd.user_photo;
-                int uploader_id =dd.uploader_id;
-                string content_text= dd.content_text;
+                int uploader_id = dd.uploader_id;
+                string content_text = dd.content_text;
                 int photo_count = dd.photo_count;
-                
+
                 int post_detail_id = dd.post_detail_id;
                 string photo = dd.photo;
 
                 tbl_post p = new tbl_post();
                 tbl_post_detail pd = new tbl_post_detail();
 
-                p.post_id =id;
+                p.post_id = id;
                 p.user_photo = user_photo;
-                p.uploader_id =uploader_id;
+                p.uploader_id = uploader_id;
                 p.content_text = content_text;
                 p.photo_count = photo_count;
 
-                pd.post_detail_id =post_detail_id;
+                pd.post_detail_id = post_detail_id;
                 pd.post_id = id;
                 pd.content_text = content_text;
                 pd.photo = photo;
 
                 _repositoryWrapper.Post_Repository.Create(p);
                 _repositoryWrapper.Post_Detail_Repository.Create(pd);
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
-                save =ex.Message;
+                save = ex.Message;
             }
             save = "Save Successfully";
             return save;
@@ -95,17 +96,17 @@ namespace Ethereal_EM
         [HttpPost("UpdatePost", Name = "UpdatePost")]
         public dynamic UpdatePost([FromBody] Newtonsoft.Json.Linq.JObject param)
         {
-            string update ="";
+            string update = "";
             try
             {
                 dynamic dd = param;
                 int id = dd.post_id;
 
                 string user_photo = dd.user_photo;
-                int uploader_id =dd.uploader_id;
-                string content_text= dd.content_text;
+                int uploader_id = dd.uploader_id;
+                string content_text = dd.content_text;
                 int photo_count = dd.photo_count;
-                
+
                 int post_detail_id = dd.post_detail_id;
                 string photo = dd.photo;
 
@@ -114,22 +115,23 @@ namespace Ethereal_EM
                 dynamic p = _repositoryWrapper.Post_Repository.GetPostByID(id);
                 dynamic pd = _repositoryWrapper.Post_Detail_Repository.GetPostDetailByID(post_detail_id);
 
-                p.post_id =id;
+                p.post_id = id;
                 p.user_photo = user_photo;
-                p.uploader_id =uploader_id;
+                p.uploader_id = uploader_id;
                 p.content_text = content_text;
                 p.photo_count = photo_count;
 
-                pd.post_detail_id =post_detail_id;
+                pd.post_detail_id = post_detail_id;
                 pd.post_id = id;
                 pd.content_text = content_text;
                 pd.photo = photo;
 
                 _repositoryWrapper.Post_Repository.Update(p);
                 _repositoryWrapper.Post_Detail_Repository.Update(pd);
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
-                update =ex.Message;
+                update = ex.Message;
             }
             update = "Update Successfully";
             return update;
@@ -141,28 +143,28 @@ namespace Ethereal_EM
             dynamic jsondata = null;
             try
             {
-                
+
                 string result = "Failed";
                 try
                 {
                     dynamic dd = param;
-                int id = dd.post_id;
+                    int id = dd.post_id;
 
-                string user_photo = dd.user_photo;
-                int uploader_id =dd.uploader_id;
-                string content_text= dd.content_text;
-                int photo_count = dd.photo_count;
-                
-                int post_detail_id = dd.post_detail_id;
-                string photo = dd.photo;
+                    string user_photo = dd.user_photo;
+                    int uploader_id = dd.uploader_id;
+                    string content_text = dd.content_text;
+                    int photo_count = dd.photo_count;
 
-                  
+                    int post_detail_id = dd.post_detail_id;
+                    string photo = dd.photo;
+
+
                     dynamic main1 = _repositoryWrapper.Post_Repository.GetPostByID(id);
                     tbl_post p = main1 as tbl_post;
 
                     dynamic main2 = _repositoryWrapper.Post_Detail_Repository.GetPostDetailByID(post_detail_id);
-                    tbl_post pd = main2 as tbl_post;
-                    
+                    tbl_post_detail pd = main2 as tbl_post_detail;
+
                     _repositoryWrapper.Post_Repository.Delete(p);
                     _repositoryWrapper.Post_Detail_Repository.Delete(pd);
                     result = "Delete Successfully";
